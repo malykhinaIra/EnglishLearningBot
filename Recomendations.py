0f173c7fc61353ee4a79f67fbde8ecfc986f8bdd
malykhinaIra <84128837+malykhinaIra@users.noreply.github.com>
2022-01-04 00:47:53 +0200

    Update Recomendations.py

--- Recomendations.py
+++ Recomendations.py
@@ -5,8 +5,8 @@ from PIL import Image
 
 class Book():
     def __init__(self, level):
-        with open("books.json","r", encoding="utf8") as books:
-          self.books = json.load(books)
+        with open("books.json", "r", encoding="utf8") as books:
+            self.books = json.load(books)
         self.level = level
         self.list_choise = list(self.books[self.level].keys())
         self.name_book = self.books[self.level][random.choice(self.list_choise)]
@@ -21,7 +21,7 @@ class Book():
 class Film():
     def __init__(self, level):
         with open("films.json", "r", encoding="utf8") as films:
-         self.films = json.load(films)
+            self.films = json.load(films)
         self.level = level
         self.list_choise = list(self.films[self.level].keys())
         self.name_film = self.films[self.level][random.choice(self.list_choise)]
@@ -36,5 +36,3 @@ class Film():
 def get_recommend(choose, level):
     dict_recommend = {"film": Film(level), "book": Book(level)}
     return dict_recommend[choose]
-
-

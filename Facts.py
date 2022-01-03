c0970ade9834cf18d96de007cbc54477cb3a356d
malykhinaIra <84128837+malykhinaIra@users.noreply.github.com>
2022-01-04 00:51:21 +0200

    new

---
+++ Facts.py
@@ -0,0 +1,16 @@
+import random
+import json
+
+def load_facts():
+    with open("facts.json", 'r', encoding="utf-8") as file:
+        fun_facts = json.load(file)
+        return fun_facts
+
+
+class Facts:
+    def __init__(self):
+        self.list_facts = load_facts()
+        self.fact = random.choice(self.list_facts)
+
+    def __str__(self):
+        return f' {self.fact} '

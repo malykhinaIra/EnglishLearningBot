e6ec012d4322143d57713213f5782a778e407dc3
malykhinaIra <84128837+malykhinaIra@users.noreply.github.com>
2022-01-04 00:48:23 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,10 +1,31 @@
 import random
+import json
+
+
+def load_questions():
+    with open("tests.json", 'r', encoding="utf-8") as file:
+        questions = json.load(file)
+        return questions
 
 
 class Test:
-    def __init__(self):
-        self.tests = ["Random test 1", "Random test 2"]
-        self.test = self.tests[random.randint(0, len(self.tests) - 1)]
+    def __init__(self, topic=None):
+        self.topic = topic
+        if not self.topic:
+            self.topic = random.randint(1, 15)
+        self.tests = load_questions()[str(self.topic)]
+        self.list_test = list(self.tests.keys())
+        self.questions = random.choice(self.list_test)
+        self.variants = self.tests[self.questions]
+        self.score = 0
+
+    def passing_test(self, answer):
+        if answer in self.variants:
+            self.score += 1
+            return f'Correct'
+
+        else:
+            return f'Wrong, correct answer is {self.variants[1]}'
 
     def __str__(self):
-        return f'{self.test}'
+        return f' {self.questions}\n{self.variants[0]} '

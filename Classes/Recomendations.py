a76b7720be09755d511a07e9e4bf21a605f27225
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2022-01-11 01:43:11 +0200

    Update Recomendations.py

--- Classes/Recomendations.py
+++ Classes/Recomendations.py
@@ -5,36 +5,75 @@ from Interfaces.IRecomendations import IRecomendations
 
 class Book(IRecomendations):
     def __init__(self, level: str):
+        self.books = load_books()
+        self.level = level
+        self.list_chose = list(self.books[self.level].keys())
+        self.name_book = self.books[self.level][random.choice(self.list_chose)]
+
+    @property
+    def level(self):
+        """Get the level of english."""
+        return self._level
+
+    @level.setter
+    def level(self, level):
+        """Set the level of english."""
         if not isinstance(level, str):
             raise TypeError('level should have type "str"')
-        with open("jsonFiles/books.json", "r", encoding="utf8") as books:
-            self.books = json.load(books)
-        self.__level = level
-        self.__list_chose = list(self.books[self.__level].keys())
-        self.__name_book = self.books[self.__level][random.choice(self.__list_chose)]
+        self._level = level
 
     def image(self) -> str:
-        return self.__name_book["image"]
+        """Get the url on cover of book."""
+        return self.name_book["image"]
 
     def description(self) -> str:
-        return f'Level: {self.__level}\n\t{self.__name_book["name"]}\n{self.__name_book["description"]}'
+        """Get the description of book."""
+        return f'Level: {self.level}\n\t{self.name_book["name"]}\n{self.name_book["description"]}'
 
 
 class Film(IRecomendations):
     def __init__(self, level):
-        with open("jsonFiles/films.json", "r", encoding="utf8") as films:
-            self.films = json.load(films)
+        self.films = load_films()
         self.level = level
         self.list_chose = list(self.films[self.level].keys())
         self.name_film = self.films[self.level][random.choice(self.list_chose)]
 
+    @property
+    def level(self):
+        """Get the level of english."""
+        return self._level
+
+    @level.setter
+    def level(self, level):
+        """Set the level of english."""
+        if not isinstance(level, str):
+            raise TypeError('level should have type "str"')
+        self._level = level
+
     def description(self):
+        """Get the description of book."""
         return f'Level: {self.level}\n\t{self.name_film["name"]}\n{self.name_film["description"]}'
 
     def image(self):
+        """Get the url on poster of film."""
         return self.name_film["image"]
 
 
 def get_recommend(choose, level):
+    """Factory function for return information about film or book """
     dict_recommend = {"film": Film(level), "book": Book(level)}
     return dict_recommend[choose]
+
+
+def load_books() -> dict:
+    """Deserialization of books.json"""
+    with open("jsonFiles/books.json", "r", encoding="utf8") as file:
+        books = json.load(file)
+        return books
+
+
+def load_films() -> dict:
+    """Deserialization of films.json"""
+    with open("jsonFiles/films.json", "r", encoding="utf8") as films:
+        films = json.load(films)
+        return films

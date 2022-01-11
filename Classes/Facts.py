dd909dc5f785e298ab61ac9a91200e58996ff457
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2022-01-11 15:31:21 +0200

    Update Facts.py

--- Classes/Facts.py
+++ Classes/Facts.py
@@ -3,16 +3,18 @@ import json
 from typing import List
 
 
-def load_facts() -> List[str]:
-    with open("jsonFiles/facts.json", 'r', encoding="utf-8") as file:
-        fun_facts = json.load(file)
-        return fun_facts
-
-
 class Facts:
     def __init__(self):
         self.list_facts = load_facts()
         self.fact = random.choice(self.list_facts)
 
     def __str__(self):
+        """Return a random fact from file"""
         return f' {self.fact} '
+
+
+def load_facts() -> List[str]:
+    """Deserialization of facts.json"""
+    with open("jsonFiles/facts.json", 'r', encoding="utf-8") as file:
+        fun_facts = json.load(file)
+        return fun_facts

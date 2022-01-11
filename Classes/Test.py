dfada8f50aa275c5bf18e6beb4cd5d4fce00c2b7
Ira Omelchenko <81821850+IraOmel@users.noreply.github.com>
2022-01-11 15:32:43 +0200

    Update Test.py

--- Classes/Test.py
+++ Classes/Test.py
@@ -8,6 +8,7 @@ from typing import List, Dict
 
 
 def load_questions() -> Dict[str, Dict[str, List[str]]]:
+    """Deserialization of tests.json"""
     with open("jsonFiles/tests.json", 'r', encoding="utf-8") as file:
         questions = json.load(file)
         return questions
@@ -18,6 +19,7 @@ class Test(ITest):
     __keyboard.add(InlineKeyboardButton(text='Розпочати', callback_data='tests'))
 
     def __init__(self, bot: TeleBot, user_id: int, topic: int = 0):
+        """Class for pass test"""
         if not isinstance(user_id, int):
             raise TypeError('user_id should have type "int"')
         if not isinstance(topic, int):
@@ -29,18 +31,18 @@ class Test(ITest):
         self.__index = topic
         if topic:
             self.__test = load_questions()[str(self.__index)]
-            self.__sentences = list(self.__test.keys())
+            self.__questions = list(self.__test.keys())
         else:
             tests = load_questions()
             self.__test = dict()
             for test in tests.values():
                 for question in test:
                     self.__test[question] = test[question]
-            self.__sentences = list(self.__test.keys())
+            self.__questions = list(self.__test.keys())
         self.__score = 0
-        self.__unused_sentences = self.__sentences
-        self.__current_sentence = None
-        self.__past_sentence = None
+        self.__unused_question = self.__questions
+        self.__current_question = None
+        self.__past_question = None
         self.__timer = Timer(bot, user_id)
         self.__time_message = None
         self.__timer.add_end_action(self.__end)
@@ -48,33 +50,31 @@ class Test(ITest):
         self.start()
 
     def next_task(self):
-        if self.__unused_sentences:
-            self.__current_sentence = random.choice(self.__unused_sentences)
-            self.__unused_sentences.remove(self.__current_sentence)
+        """Do the next question"""
+        if self.__unused_question:
+            self.__current_question = random.choice(self.__unused_question)
+            self.__unused_question.remove(self.__current_question)
 
-    def is_right(self, answer: str) -> bool:
-        if not isinstance(answer, str):
-            raise TypeError('answer should have type "str"')
-        return self.__test[self.__current_sentence][1] == answer
 
     def passing_test(self, answer: str) -> str:
-        if not self.__current_sentence:
-            raise ValueError('there are no task to answer')
-        right_answer = self.__test[self.__current_sentence][1]
-        self.__past_sentence = self.__current_sentence
-        self.__current_sentence = None
+        """Return result (answer correct or not), count user's score"""
+        right_answer = self.__test[self.__current_question][1]
+        self.__past_question = self.__current_question
+        self.__current_question = None
         if answer == right_answer:
             self.__score += 1
             return f'Correct'
         return f'Wrong, correct answer is {right_answer}'
 
     def __str__(self):
-        if not self.__current_sentence:
+        """Return questions with answer options"""
+        if not self.__current_question:
             raise ValueError('there are no task')
-        return f' {self.__current_sentence}\n{self.__test[self.__current_sentence][0]} '
+        return f' {self.__current_question}\n{self.__test[self.__current_question][0]} '
 
     @classmethod
     def get_keyboard(cls) -> InlineKeyboardMarkup:
+        """Get keyboard with button to start the test"""
         return cls.__keyboard
 
     @property
@@ -83,50 +83,56 @@ class Test(ITest):
 
     @property
     def score(self) -> int:
+        """Get score of passing test"""
         return self.__score
 
     @property
     def questions_count(self):
-        return len(self.__sentences)
+        return len(self.__questions)
 
     @property
     def sentence(self) -> str:
-        return self.__current_sentence
+        return self.__current_question
 
     @property
     def last_sentence(self) -> str:
-        return self.__past_sentence
+        return self.__past_question
 
     @property
     def is_started(self) -> bool:
         return self.__timer.is_started
 
     def delete(self):
+        """Delete timer-message"""
         if self.__timer.is_started:
             self.stop()
         del self
 
     def start(self):
+        """Starts the timer"""
         self.next_task()
         self.stop()
-        self.__time_message = self.__bot.send_message(self.__user_id, 'Лишилось 10 хв')
+        self.__time_message = self.__bot.send_message(self.__user_id, 'Залишилось 10 хв')
         self.__timer.start(10)
 
     def __end(self):
+        """Send message if time run out"""
         self.__bot.send_message(self.__user_id, f'Час вийшов')
         self.__bot.delete_message(self.__user_id, self.__time_message.message_id)
         self.__time_message = None
         self.__bot.send_message(self.__user_id, "Новий тест", reply_markup=[Test.get_keyboard()])
 
     def __update(self, minutes):
+        """Updating of timer"""
         if minutes == 0:
-            self.__bot.edit_message_text('Лишилось менше 30 секунд', self.__user_id, self.__time_message.message_id)
+            self.__bot.edit_message_text('Залишилось менше 30 секунд', self.__user_id, self.__time_message.message_id)
         else:
             self.__bot.edit_message_text(
-                f'лишил{"a" if int(minutes * 2) == 3 else "o"}сь {minutes} хв',
+                f'Залишил{"a" if int(minutes * 2) == 3 else "o"}сь {minutes} хв',
                 self.__user_id, self.__time_message.message_id)
 
     def stop(self):
+        """Stop the timer"""
         if self.__timer.is_started:
             self.__bot.delete_message(self.__user_id, self.__time_message.message_id)
             self.__time_message = None
